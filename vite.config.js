7f761f4554456fdbc3a76413f29aebd6105fb8fb
MohamedMohamed <m01126165016@icloud.com>
2024-08-30 01:07:00 +0300

    deploy config

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,10 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/",
   resolve: {
     alias: {
       "@": "/src",
     },
   },
-  base: "/ChatGPT/",
 });
