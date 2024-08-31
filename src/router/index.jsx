cfa96a9cdb9085e959a5a06f5e5c5a0d76870c40
MohamedMohamed <m01126165016@icloud.com>
2024-08-31 18:53:14 +0300

    Test

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -1,4 +1,4 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 
 // Pages
 import { Welcome } from "@/pages/Welcome";
@@ -6,27 +6,17 @@ import { Login } from "@/pages/Login";
 import { StartScreen } from "@/pages/StartScreen";
 import { SignUp } from "@/pages/SignUp/SignUp";
 
-const router = createBrowserRouter([
-  {
-    path: "/ChatGPT/",
-    element: <Welcome />,
-  },
-  {
-    path: "/ChatGPT/login",
-    element: <Login />,
-  },
-  {
-    path: "/ChatGPT/signup",
-    element: <SignUp />,
-  },
-  {
-    path: "/ChatGPT/start",
-    element: <StartScreen />
-  },
-]);
-
 const Router = () => {
-  return <RouterProvider router={router} />;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/ChatGPT/" element={<Welcome />} />
+        <Route path="/ChatGPT/login" element={<Login />} />
+        <Route path="/ChatGPT/signup" element={<SignUp />} />
+        <Route path="/ChatGPT/start" element={<StartScreen />} />
+      </Routes>
+    </Router>
+  );
 };
 
 export default Router;
